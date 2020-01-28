10bffa00b0662c8a5d0b535cc4d5545be22514db
IMJIE <imjie@outlook.com>
2020-01-28 17:50:21 +0800

    update

--- runtest.py
+++ runtest.py
@@ -8,14 +8,3 @@ if __name__ == '__main__':
     project = Project()
 
     project.start()
-
-# def test(test,test1):
-#     print(test,test1)
-#     return '{}'.format(test+test1)
-#
-# ex = futures.ThreadPoolExecutor()
-# worker_list = []
-# worker_list.append(ex.submit(test(test1=1,test=2)))
-# for f in futures.as_completed(worker_list):
-#     # if f.result() is not None:
-#     print(f)
