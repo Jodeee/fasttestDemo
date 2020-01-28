cf6da432a6815f0ac28eb00770bb9cf562dc7ada
IMJIE <imjie@outlook.com>
2020-01-28 17:49:49 +0800

    first submit

--- runtest.py
+++ runtest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from  utest import *
+from oktest import *
+from concurrent import futures
 
 if __name__ == '__main__':
 
@@ -8,6 +9,13 @@ if __name__ == '__main__':
 
     project.start()
 
-
-
-
+# def test(test,test1):
+#     print(test,test1)
+#     return '{}'.format(test+test1)
+#
+# ex = futures.ThreadPoolExecutor()
+# worker_list = []
+# worker_list.append(ex.submit(test(test1=1,test=2)))
+# for f in futures.as_completed(worker_list):
+#     # if f.result() is not None:
+#     print(f)
